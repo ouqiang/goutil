82b3d2997d337a2174b708d0ffe3f711ffb1eddf
ouqiang <ouqiang@4399inc.com>
2018-04-18 09:28:05 +0800

    增加方法: 获取程序运行时工作目录

--- util.go
+++ util.go
@@ -25,6 +25,7 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"path/filepath"
 	"runtime"
 	"time"
 )
@@ -105,4 +106,18 @@ func DownloadFile(filePath string, rw http.ResponseWriter) error {
 	_, err = io.Copy(rw, file)
 
 	return err
+}
+
+// WorkDir 获取程序运行时根目录
+func WorkDir() (string, error) {
+	execPath, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	wd := filepath.Dir(execPath)
+	if filepath.Base(wd) == "bin" {
+		wd = filepath.Dir(wd)
+	}
+
+	return wd, nil
 }
