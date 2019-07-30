292f36867b5487b08425c5882affbfc85554a41d
ouqiang <ouqiang@4399inc.com>
2019-07-30 10:14:29 +0800

    增加pid文件写入读取

---
+++ pid/pid.go
@@ -0,0 +1,45 @@
+package pid
+
+import (
+	"errors"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"syscall"
+)
+
+// WriteToFile 写入pid到文件中
+func WriteToFile(filename string) error {
+	pid := os.Getpid()
+	if pid <= 0 {
+		return errors.New("failed to get pid")
+	}
+	err := ioutil.WriteFile(filename, []byte(strconv.Itoa(pid)), 0644)
+
+	return err
+}
+
+// ReadFromFile 从文件中读取pid
+func ReadFromFile(filename string) (int64, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return 0, err
+	}
+	pid, _ := strconv.Atoi(string(buf))
+	if pid <= 0 {
+		return 0, errors.New("failed to parse pid")
+	}
+
+	return int64(pid), nil
+}
+
+// IsRunning 通过pid判断进程是否运行中
+func IsRunning(pid int) bool {
+	process, err := os.FindProcess(pid)
+	if err != nil {
+		return false
+	}
+	err = process.Signal(syscall.Signal(0))
+
+	return err == nil
+}
---
+++ pid/pid_test.go
@@ -0,0 +1,32 @@
+package pid
+
+import (
+	"io/ioutil"
+	"math"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestReadWriteToFile(t *testing.T) {
+	f, err := ioutil.TempFile("", "pid")
+	require.NoError(t, err)
+	_ = f.Close()
+	defer func() {
+		_ = os.Remove(f.Name())
+	}()
+	t.Logf("temp file: %s, pid: %d", f.Name(), os.Getpid())
+	err = WriteToFile(f.Name())
+	require.NoError(t, err)
+
+	pid, err := ReadFromFile(f.Name())
+	require.NoError(t, err)
+
+	require.Equal(t, os.Getpid(), int(pid))
+}
+
+func TestIsRunning(t *testing.T) {
+	require.Equal(t, true, IsRunning(os.Getpid()))
+	require.Equal(t, false, IsRunning(math.MaxInt32))
+}
