16cc80e0ce10498ab9a1dc4c6f720ab2b319b5f7
ouqiang <ouqiang@4399inc.com>
2019-12-25 14:40:11 +0800

    增加sha1流处理

--- crypt/sha1.go
+++ crypt/sha1.go
@@ -3,6 +3,8 @@ package crypt
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"io"
+	"os"
 )
 
 // SHA1 生成sha1摘要
@@ -11,4 +13,27 @@ func SHA1(s string) string {
 	_, _ = m.Write([]byte(s))
 
 	return hex.EncodeToString(m.Sum(nil))
+}
+
+// SHA1Stream 流式处理
+func SHA1Stream(reader io.Reader) (string, error) {
+	m := sha1.New()
+	_, err := io.Copy(m, reader)
+	if err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(m.Sum(nil)), nil
+}
+
+func SHA1StreamSum(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	return SHA1Stream(file)
 }
--- crypt/sha1_test.go
+++ crypt/sha1_test.go
@@ -1,6 +1,7 @@
 package crypt
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -16,4 +17,11 @@ func BenchmarkSHA1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		SHA1(s)
 	}
+}
+
+func TestSHA1Stream(t *testing.T) {
+	s := "goutil"
+	result, err := SHA1Stream(strings.NewReader(s))
+	require.NoError(t, err)
+	require.Equal(t, "05cb8eaebc8f5d3514449b4f6929ae0fb6a036a1", result)
 }
