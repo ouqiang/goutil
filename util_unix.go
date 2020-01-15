9c919f81117506d93cc4244b922640cfb273976e
ouqiang <ouqiang@4399inc.com>
2020-01-15 16:04:04 +0800

    守护进程从os.Args[0]获取可执行文件路径

--- util_unix.go
+++ util_unix.go
@@ -1,35 +1,36 @@
 package goutil
 
 import (
-        "github.com/ouqiang/goutil/slice"
-        "os"
-        "os/exec"
-        "syscall"
-        "io"
+	"io"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/ouqiang/goutil/slice"
 )
 
 // Daemon  守护进程
 func Daemon(w io.Writer) {
-        execFile, err := os.Executable()
-        if err != nil {
-                panic(err)
-        }
-        args := slice.Remove(os.Args[1:], "-d")
-        cmd := exec.Command(execFile, args...)
-        cmd.Stdin = nil
-        if w != nil {
-                cmd.Stdout = w
-                cmd.Stderr = w
-        } else {
-                cmd.Stdout = nil
-                cmd.Stderr = nil
-        }
-        cmd.SysProcAttr = &syscall.SysProcAttr{
-                Setsid: true,
-        }
-        err = cmd.Start()
-        if err != nil {
-                panic(err)
-        }
-        os.Exit(0)
+	execFile, err := exec.LookPath(os.Args[0])
+	if err != nil {
+		panic(err)
+	}
+	args := slice.Remove(os.Args[1:], "-d")
+	cmd := exec.Command(execFile, args...)
+	cmd.Stdin = nil
+	if w != nil {
+		cmd.Stdout = w
+		cmd.Stderr = w
+	} else {
+		cmd.Stdout = nil
+		cmd.Stderr = nil
+	}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setsid: true,
+	}
+	err = cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	os.Exit(0)
 }
