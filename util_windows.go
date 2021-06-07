5cf272b4cb57b519d4ca7411f007965101b5baff
ouqiang <ouqiang@4399inc.com>
2021-06-07 16:30:26 +0800

    windows守护进程预留空方法

---
+++ util_windows.go
@@ -0,0 +1,15 @@
+/**
+ * Created by GoLand.
+ * User: ouqiang<ouqiang@4399inc.com>
+ * Date: 2021/6/7
+ * Time: 16:27
+ */
+
+package goutil
+
+import (
+	"io"
+)
+
+// Daemon  守护进程 不支持windows
+func Daemon(io.Writer) {}
